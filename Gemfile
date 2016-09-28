6b53b39f7c90bbc44a7ada3d1f11f60887caafbd
justinweathersby <justinweathersby@gmail.com>
2016-09-28 16:19:25 -0400

    Started to add dealerships

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'devise'
 gem 'bootstrap', '~> 4.0.0.alpha4'
 gem "font-awesome-rails"
 
+gem "paperclip", "~> 5.0.0"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928062902) do
+ActiveRecord::Schema.define(version: 20160928155001) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,32 @@ ActiveRecord::Schema.define(version: 20160928062902) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "dealerships", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "primary_color"
+    t.string   "new_cars_url"
+    t.string   "used_cars_url"
+    t.string   "service_url"
+    t.string   "sales_email"
+    t.string   "service_email"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "zipcode"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+    t.string   "background_image_file_name"
+    t.string   "background_image_content_type"
+    t.integer  "background_image_file_size"
+    t.datetime "background_image_updated_at"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
 end
