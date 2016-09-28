a2d91c9a04299e51c091012e33714488d9301531
justinweathersby <justinweathersby@gmail.com>
2016-09-28 03:25:02 -0400

    Setup authentication

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_admin_user!
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
+  devise_for :admin_users, path_names:  { sign_in: "login", sign_out: "logout" },
+                     controllers: {
+                                    registrations: "admin_user/registrations"
+                                  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
