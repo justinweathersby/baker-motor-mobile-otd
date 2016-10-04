81ae1ae278ee0a398bd346360e8e0ab2af084f38
justinweathersby <justinweathersby@gmail.com>
2016-10-04 10:53:03 -0400

    Polished sign up page

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -40,7 +40,7 @@ before_action :configure_account_update_params, only: [:update]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :dealership_id]])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :dealership_id])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
