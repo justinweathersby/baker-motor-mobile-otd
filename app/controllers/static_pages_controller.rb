f61a6644c9e48afb75e0ca6671045402d0bbb69f
justinweathersby <justinweathersby@gmail.com>
2016-10-04 03:37:36 -0400

    Setup index dashboard for every role

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,9 @@ class StaticPagesController < ApplicationController
   def index
     @moderators = User.with_role(:moderator, :any)
     @customers = User.with_role(:customer)
+
+    if user_signed_in? and current_user.has_role?(:moderator, :any)
+      @dealership = Dealership.with_role(:moderator, current_user).first
+    end
   end
 end
