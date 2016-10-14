914cb22ed65a6a3e96432a3ec769edc1295301c4
justinweathersby <justinweathersby@gmail.com>
2016-10-13 20:03:09 -0400

    Problem with logout only on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, path_names:  { sign_in: "login", sign_out: "logout" },
                      controllers: {
-                                    registrations: "user/registrations"
+                                    registrations: "user/registrations",
+                                    sessions: "user/sessions"
                                   }
 
   #===============Api Routes================
