696d19eb806af06f5eb43d7baa3e18e8e2eb3a28
justinweathersby <justinweathersby@gmail.com>
2016-10-13 20:51:23 -0400

    Removed rails 12 factor gem

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module BakerMotorMobileOtd
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.serve_static_assets = true
+    
     config.middleware.insert_before 0, "Rack::Cors" do
      allow do
        origins '*'
